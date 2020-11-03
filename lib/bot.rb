c18adc3885a6e2885a7707f59056e069ed053700
Leonardo <fanger53@gmail.com>
2020-11-02 22:55:16 -0500

    add test file for review individual part of bot

--- lib/bot.rb
+++ lib/bot.rb
@@ -15,7 +15,7 @@ class Bot_l
           bot.api.send_message(chat_id: l_options.chat.id,text: "#{ Bot_s.new}")
         when '/stop'
           key_board = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-          bot.api.send_message(chat_id: l_options.chat.id, text: "Thanks for use my cooking advices", reply_markup: key_board)
+          bot.api.send_message(chat_id: l_options.chat.id, text: "i will go to the bed", reply_markup: key_board)
         else 
           bot.api.send_message(chat_id: l_options.chat.id,text: "Wrong entry try to use /star, /stop, /English, /Spanish  ") 
       end
@@ -33,11 +33,13 @@ class Bot
       case bot_options.text
         when '/start' 
           bot.api.send_message(chat_id: bot_options.chat.id, text: "Hello, #{bot_options.from.first_name},Im Alfred im here to help u to have good diet, like the helper of mr wayne i always try to do the best")
-        when '/music'
-          music
+        when 'food'
+          food
+        when 'inic'
+          bot.api.send_message(chat_id: bot_options.chat.id,text: "volver al inicio #{Bot_l.new} ") 
         when '/stop'
           key_board = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-          bot.api.send_message(chat_id: bot_options.chat.id, text: "Thanks for use my cooking advices", reply_markup: key_board)
+          bot.api.send_message(chat_id: bot_options.chat.id, text: "Thanks for use my cooking advices ", reply_markup: key_board)
         else 
           bot.api.send_message(chat_id: bot_options.chat.id,text: "Wrong entry try to use /start, /stop, /English, /Spanish  ") 
       end
@@ -55,37 +57,38 @@ class Bot_s
       case bot_options.text
         when '/start' 
           bot.api.send_message(chat_id: bot_options.chat.id, text: "Hello, #{bot_options.from.first_name},hola soy alfred ")
-        when '/music'
-          music
+        when 'food'
+          food
         when '/stop'
           key_board = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
           bot.api.send_message(chat_id: bot_options.chat.id, text: "Thanks for use my cooking advices", reply_markup: key_board)
         else 
-          bot.api.send_message(chat_id: bot_options.chat.id,text: "Wrong entry try to use /start(empezar), /stop, /English, /Spanish  ") 
+          bot.api.send_message(chat_id: bot_options.chat.id,text: "Wrong entry try to use /start(empezar), /stop ") 
       end
     end
   end
   end
 
-  def music
+  def food
     @token = '1495663141:AAFFpexoWNH-GJq1HlbIO3e3ObECgKnTeoI'
     Telegram::Bot::Client.run(@token) do |bot|
-      bot.listen do |message|
+      bot.listen do |message|  
         case message.text
-        when '/meal'
-          answers = Telegram::Bot::Types::ReplyKeyboardMarkup
-            .new(keyboard: [%w[Breakfast Lunch], %w[Dinner Elevenses]], one_time_keyboard: true)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Select what you want to eat', reply_markup: answers)
-        when '/breakfast'
+          when 'food'
+            bot.api.send_message(chat_id: message.chat.id, text: 'Select what you want to eat')
+        when 'breakfast'
           bot.api.send_message(chat_id: message.chat.id, text: @select.rand_select(@select.breakfast))
-        when '/lunch'
+        when 'lunch'
           bot.api.send_message(chat_id: message.chat.id, text: @select.rand_select(@select.lunch))
-        when '/dinner'
+        when 'dinner'
           bot.api.send_message(chat_id: message.chat.id, text: @select.rand_select(@select.dinner))
-        when '/elevenses'
+        when 'elevenses'
           bot.api.send_message(chat_id: message.chat.id, text: @select.rand_select(@select.elevenses))
+        when 'stop'
+          key_board = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
+          bot.api.send_message(chat_id: message.chat.id, text: "Thanks for use my cooking advices", reply_markup: key_board)
         else
-          bot.api.send_message(chat_id: message.chat.id, text: "choose your meal btwn /breakfast, /lunch, /dinner, /elevenses")
+          bot.api.send_message(chat_id: message.chat.id, text: "choose your meal btwn breakfast, lunch, dinner, elevenses")
         end
       end
     end
